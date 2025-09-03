4c38562a9682721e98cc2378c1dcd93bb6c5ae64
EmilyNorena <emily.norena-c@mail.escuelaing.edu.co>
2025-09-03 10:54:59 -0500

    parte 2

--- src/main/java/edu/eci/blacklistvalidator/threads/HostBlackListThread.java
+++ src/main/java/edu/eci/blacklistvalidator/threads/HostBlackListThread.java
@@ -1,5 +1,6 @@
 
 package edu.eci.blacklistvalidator.threads;
+
 import java.util.LinkedList;
 import java.util.List;
 
@@ -18,12 +19,19 @@ public class HostBlackListThread extends Thread {
     private final String ip;
     private final HostBlacklistsDataSourceFacade skds;
     private final List<Integer> localOccurrences;
+    private static volatile boolean finished = false;
+    private static final int BLACK_LIST_ALARM_COUNT = 5;
 
     /*
-     * Constructor to initialize the thread with its range, IP, and data source facade.
+     * Constructor to initialize the thread with its range, IP, and data source
+     * facade.
+     * 
      * @param startIdx Start index of the blacklist range (inclusive)
+     * 
      * @param endIdx End index of the blacklist range (exclusive)
+     * 
      * @param ip IP address to check
+     * 
      * @param skds HostBlacklistsDataSourceFacade to query blacklist servers
      */
     public HostBlackListThread(int startIdx, int endIdx, String ip, HostBlacklistsDataSourceFacade skds) {
@@ -41,15 +49,20 @@ public class HostBlackListThread extends Thread {
      */
     @Override
     public void run() {
-        for (int i = startIdx; i < endIdx; i++) {
+        for (int i = startIdx; i < endIdx && !finished; i++) {
             if (skds.isInBlackListServer(i, ip)) {
                 localOccurrences.add(i);
+                if (localOccurrences.size() >= BLACK_LIST_ALARM_COUNT) {
+                    finished = true;
+                    return;
+                }
             }
         }
     }
 
     /*
      * Returns the list of blacklist server indices where the IP was found.
+     * 
      * @return List<Integer> containing server indices
      */
     public List<Integer> getOccurrences() {
